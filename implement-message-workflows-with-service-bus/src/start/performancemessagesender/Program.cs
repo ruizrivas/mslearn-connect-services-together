0d4739a195ed3260bd757194a09e9d6a49cf2a6f
Francisco Ruiz Rivas <francisco@ruizrivas.net>
2022-03-01 21:51:09 -0300

    Send a message to the topic

--- implement-message-workflows-with-service-bus/src/start/performancemessagesender/Program.cs
+++ implement-message-workflows-with-service-bus/src/start/performancemessagesender/Program.cs
@@ -6,8 +6,8 @@ namespace performancemessagesender
 {
     class Program
     {
-        const string ServiceBusConnectionString = "";
-        const string TopicName = "salesperformancemessages";
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamappfar2022.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=kYS+c5YkJaM2RbsoJ0+N4SLsUcT/Ig0T9tXYsPvEIVw=";
+        const string TopicName = "salesperformancemessages1";
 
         static void Main(string[] args)
         {
@@ -24,12 +24,23 @@ namespace performancemessagesender
         {
             // Create a Service Bus client here
 
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
             // Create a sender here
+
+            await using var sender = client.CreateSender(TopicName);
 
             // Send messages.
             try
             {
                 // Create and send a message here
+
+                var messageBody = "Total sales for Argentina in August: $13m.";
+                var message = new ServiceBusMessage(messageBody);
+
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                await sender.SendMessageAsync(message);
             }
             catch (Exception exception)
             {
